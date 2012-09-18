d28b2df11d0797f5b43c32c2fe460a684ceaad1b
Rick Seufert <rseufert@usbmis.com>
2012-09-18 17:57:37 -0300

    Cleaned up variable declarations
    Cleaned up variable declarations and added comments

--- age_in_days/age_in_days.js
+++ age_in_days/age_in_days.js
@@ -1,22 +1,26 @@
 var age = prompt('How old are you?');
 var birthMonth = prompt('Please enter the number of your birth month?');
 var birthDay = prompt('What day of the month?');
+var birthDate;
+var ageMill;
+var dateMill;
+var numDays;
 
 var date = new Date();
 var birthYear = date.getFullYear() - age;
 var m = date.getMonth() + 1;
 var d = date.getDate();
 
-if (birthMonth > m || (birthMonth == m && birthDay > d)){
+if (birthMonth > m || (birthMonth == m && birthDay > d)){  //calculate birth year
   birthYear--;
 }
 
-var birthDate = new Date(birthYear, birthMonth - 1, birthDay);
-var ageMill = birthDate.getTime();
-var dateMill = date.getTime();
-ageMill = dateMill - ageMill;
+birthDate = new Date(birthYear, birthMonth - 1, birthDay);  //create date object for birth date
+ageMill = birthDate.getTime();                              //get birthdate in milliseconds
+dateMill = date.getTime();                                  //get date in milliseconds
+ageMill = dateMill - ageMill;                               //get age in milliseconds
 
-var numDays = Math.round(ageMill / 1000 / 60 / 60 / 24);
+numDays = Math.round(ageMill / 1000 / 60 / 60 / 24);        //convert to days
 
 
 alert("You are " + numDays + " days old.");
