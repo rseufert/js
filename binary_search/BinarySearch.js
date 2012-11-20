6e42d474d3baf64a3414540c21ce855400dfea66
Rick Seufert <rseufert@usbmis.com>
2012-11-20 15:44:57 -0500

    Revert "Many changes. See extended description"
    This reverts commit d90b79bb662ca4cdb484021bc4a903e78537937c.

--- binary_search/BinarySearch.js
+++ binary_search/BinarySearch.js
@@ -22,61 +22,39 @@ var data = [];  //ordered array of values
     var animateIndex = 0;   //used to "playback" the values in searchState
 
 
-    var clear = function() {          //clears the canvas, sets the background color
+    function clear() {          //clears the canvas
         c.fillStyle = "D1D9E8";
         c.fillRect(0, 0, 1000, 500);
-    };
+    }
 
 
-    var drawData = function(d) {          //need to draw the remaining elements in the data array
+    function drawData(d) {          //draw data
         clear();
         c.fillStyle = "7D879B"; 
         for(var i = 0; i < d.length; i++) { 
             var dp = d[i]; 
-            c.fillRect(25 + i*100, 500 - dp*5 - 30, 50, dp*5); 
+            c.fillRect(25 + i*100, 500 - dp*5 - 30 , 50, dp*5); 
         }
-    };
+    }
 	
-
-    var drawResult = function(d, r) {     //similar to draw data, but highlights the search result
+    function drawResult(d, r) {     //draws all of the data with the search result highlighted
         clear();
         c.fillStyle = "7D879B"; 
         for(var i = 0; i < d.length; i++) { 
             var dp = d[i]; 
             if (i === r){
                 c.fillStyle = "EC8A6F";
-                c.fillRect(25 + i*100, 500 - dp*5 - 30, 50, dp*5);
+                c.fillRect(25 + i*100, 500 - dp*5 - 30 , 50, dp*5);
                 c.fillStyle = "7D879B";
             } else {
-                c.fillRect(25 + i*100, 500 - dp*5 - 30, 50, dp*5); 
+                c.fillRect(25 + i*100, 500 - dp*5 - 30 , 50, dp*5); 
             }
         }
-    };
+    }
 
 
-    var animate = function() {        //draw the appropriate frame each time the user clicks Next
-        if(animateIndex < stateIndex) {
-            for(var i = 0; i < searchState[animateIndex].length; i++) {
-                drawData(searchState[animateIndex]);
-            }
-            animateIndex++;
-        } else {
-            drawResult(data, searchResult);
 
-            if (searchResult === -1) {
-                document.getElementById("found").innerHTML = "";
-                document.getElementById("not_found").innerHTML = "Sorry, the number you are looking for is not in the array.";
-                document.getElementById("i").innerHTML = "<img src='i' />";
-            } else {
-                document.getElementById("not_found").innerHTML = "";
-                document.getElementById("i").innerHTML = "";
-                document.getElementById("found").innerHTML = "The number you are looking for is at array position: " + searchResult;
-            }    
-        }
-    };
-
-
-    var binarySearch = function(searchTerm, data, left, right) {
+    function binarySearch(searchTerm, data, left, right) {
         if (left > right)
             return -1;
 
@@ -84,7 +62,7 @@ var data = [];  //ordered array of values
         console.log(searchState[stateIndex]);
         stateIndex++;
 	 
-        var mid = Math.round((left + right) / 2);
+        var mid = parseInt((left + right) / 2);
 	 
         if (data[mid] === searchTerm) {
             return mid;
@@ -93,23 +71,32 @@ var data = [];  //ordered array of values
         } else {
             return binarySearch(searchTerm, data, mid + 1, right);
         }
-    };
+    }
 
 
-    var search = function(term) {
-        console.log("Searching for " + term);
-        searchResult = binarySearch(term, data, 0, data.length - 1);
-        console.log(searchResult);
-        document.getElementById("next").innerHTML = "<button onclick='animate();'>Next</button>";
-    };
+    function search() {             //calls binarySearch then animates the results
+        function animate() {        //iterates through the array of changes and draws each state
+            if(animateIndex < stateIndex) {
+                for(var i = 0; i < searchState[animateIndex].length; i++) {
+                    drawData(searchState[animateIndex]);
+                }
+                animateIndex++;
+            } else {
+                drawResult(data, result);
+                window.clearInterval(intId);
+            }
+        }
 
-
-    var readForm = function() {
-        var search_term = document.search.term.value;
-        search_term = parseInt(search_term, 10);
-        search(search_term);
-    };
-
-
+        var result = binarySearch(75, data, 0, data.length - 1);
+        console.log(result);
+        var intId = window.setInterval(animate, 1750);
+        if (result === -1) {
+            document.write("<br /><br /> <b>***Spoiler Alert*** Sorry, the number you are looking for is not in the array.</b> \n");
+            document.write("<br /><br /> <img src='i' />");
+        } else {
+            document.write("<br /><br /> <b>***Spoiler Alert*** The number you are looking for is at array position: " + result + "</b>");
+        }
+    }
+	
     clear();    //initial clear to set the background color (I'm anal...)
-    document.getElementById("search").innerHTML = "<form name='search'>Search: <input type='text' id='term' name='term'></form><button name='searchButton' onclick='readForm();'>Search</button>";
+    search();   //Makes magics happens!!1
