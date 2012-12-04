711fc034263fd34e489ca8f4f96a8ba0efabd20d
Rick Seufert <rseufert@usbmis.com>
2012-12-04 12:21:02 -0500

    Implemented basic algorithm and set up project
    * Set up the project w/ reusable code from BinarySearch
    * Configured CSS
    * Implemented bubble sort algorithm

--- bubble_sort/BubbleSort.js
+++ bubble_sort/BubbleSort.js
@@ -1,4 +1,4 @@
-    var data = [];     //array of data to be sorted
+    var data = [67, 43, 50, 29, 37, 13, 80, 74, 47, 111, 26, 32, 58, 99, 21, 34, 95];     //array of data to be sorted
 
 
     //get a reference to the canvas 
@@ -11,21 +11,23 @@
     var canvasWidth = canvas.getAttribute("width");
     var canvasHeight = canvas.getAttribute("height");
 
-    var canvasColor = "D1D9E8";
-    var barColorNormal = "7D879B";
+    var canvasColor = "BBDDF2";
+    var barColorNormal = "0B3B59";
     var barWidth = 50;
-    var barSpacing = 85;
+    var barSpacing = 70;
     var leftOffset = 20;
     var bottomOffset = 2;
     var heightMultiple = 5;
-    var labelColor = "white";
-    var labelFont = "18pt sans-serif";
+    var labelColor = "E2F266";
+    var labelFont = "18pt Lucida Sans";
     
 
-    var clear = function() {          //clears the canvas, sets the background color
+
+    var clear = function() {                               //clears the canvas, sets the background color
         c.fillStyle = canvasColor;
         c.fillRect(0, 0, canvasWidth, canvasHeight);
     };
+    
 
     var drawData = function(d, optionalColor) {          //need to draw the remaining elements in the data array
         optionalColor = (typeof optionalColor === "undefined") ? barColorNormal : optionalColor;
@@ -40,4 +42,38 @@
         }
     };
 
-    clear();                                            //initial clear to set the background 
+
+    var bubbleSort = function(d) {                      //iterates through the array, compares values, moves higher value one position to the right...
+        var len = d.length;
+        var bound = len - 1;
+        var newBound = 0;
+        var temp;
+        console.log("Starting array: " + d);
+        for (var i = 1; i <= len; i++) {
+            newBound = 0;
+            for (var j = 0; j <= bound; j++) {
+                if (d[j] > d[j +1 ]) {
+                    temp = d[j];                        //if left value is greater than right value, use temp variable to swap the values
+                    d[j] = d[j + 1];
+                    d[j + 1] = temp;
+                    newBound = j - 1;
+                    console.log(d);
+                }
+            }
+            bound = newBound;
+        }
+        console.log("Sorted array: " + d);
+        return d;
+    };
+
+
+    var sortData = function() {
+        data = bubbleSort(data);
+        drawData(data);
+    };
+    
+
+    clear();                                            //initial clear to set the background
+    drawData(data);
+    //data = bubbleSort(data);
+    //drawData(data)
